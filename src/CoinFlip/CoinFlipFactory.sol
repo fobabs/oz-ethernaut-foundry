17241cae89151db696c1de485d832b30352a1130
FOBABS <fobabs2win@rocketmail.com>
2025-04-21 00:20:36 +0100

    CoinFlip

---
+++ src/CoinFlip/CoinFlipFactory.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Level} from "../Level.sol";
+import {CoinFlip} from "./CoinFlip.sol";
+
+
+contract CoinFlipFactory is Level {
+    function createInstance(address /*_player*/) public payable override returns (address) {
+        CoinFlip coinFlip = new CoinFlip();
+        return address(coinFlip);
+    }
+
+    function validateInstance(address payable _instance, address /*_player*/) public view override returns (bool) {
+        CoinFlip instance = CoinFlip(_instance);
+        return instance.consecutiveWins() >= 10;
+    }
+}
---
+++ test/CoinFlip.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {BaseTest} from "./BaseTest.t.sol";
+import {CoinFlip} from "../src/CoinFlip/CoinFlip.sol";
+import {CoinFlipFactory} from "../src/CoinFlip/CoinFlipFactory.sol";
+import {CoinFlipAttacker} from "../src/CoinFlip/CoinFlipAttacker.sol";
+
+
+contract CoinFlipTest is BaseTest {
+    function testCoinFlipHack() public {
+        ////////////////////*/
+        //    Level Setup
+        ////////////////////*/
+        CoinFlipFactory factory = new CoinFlipFactory();
+        ethernaut.registerLevel(factory);
+        vm.startPrank(player);
+        address levelAddress = ethernaut.createLevelInstance(factory);
+        CoinFlip ethernautCoinFlip = CoinFlip(payable(levelAddress));
+
+        ////////////////////*/
+        //   Level Attack
+        ////////////////////*/
+        CoinFlipAttacker coinFlipAttacker = new CoinFlipAttacker(levelAddress);
+        // Run the attack 10 times
+        for (uint256 i = 0; i < 10; i++) {
+            vm.roll(i + 1);
+            coinFlipAttacker.attack();
+        }
+        assert(ethernautCoinFlip.consecutiveWins() >= 10);
+
+        //////////////////////*/
+        //  Level Submission
+        //////////////////////*/
+        bool levelCompleted = ethernaut.submitLevelInstance(payable(levelAddress));
+        vm.stopPrank();
+        assertTrue(levelCompleted);
+    }
+}
